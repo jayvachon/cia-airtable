54fa8d8dc9cc735199654f5ede83de4571c0c1a8
jayvachon <jaylvachon@gmail.com>
2021-03-01 13:24:37 -0500

    include email signature in auto-emails

--- templates.js
+++ templates.js
@@ -1,5 +1,11 @@
 const _ = require('lodash');
 
+const signature = `<div><br></div>
+	--
+	<br>
+	<div dir="ltr"><div dir="ltr"><div><div dir="ltr"><div><div dir="ltr"><div dir="ltr"><div dir="ltr"><div dir="ltr"><div dir="ltr"><div dir="ltr"><div dir="ltr"><div dir="ltr"><div dir="ltr"><div dir="ltr"><div dir="ltr"><div dir="ltr"><div dir="ltr"><div dir="ltr"><div dir="ltr"><div dir="ltr"><div dir="ltr"><div dir="ltr"><div dir="ltr"><div dir="ltr"><div dir="ltr"><div dir="ltr"><div dir="ltr"><div dir="ltr"><div style="font-size:small"><span style="color:rgb(12,52,61)"><font face="tahoma, sans-serif">Department of Admissions</font></span></div><div style="font-size:small"><div style="font-size:12.8px"><div style="font-size:small"><font size="2" color="#0c343d" face="tahoma, sans-serif">Code Immersives NYC&nbsp;</font></div><div style="font-size:small"><font size="2" color="#0c343d" face="tahoma, sans-serif">630 Ninth Ave (Suite 901)<br>New York, NY 10036<br>F: (917) 398-9853<br>T: (646) 215-2200 ext: 1014<br></font></div><div style="font-size:small"><font size="2" color="#0000ff" face="tahoma, sans-serif"><b><br></b></font></div><div style="font-size:small"><font size="2" color="#0c343d" face="tahoma, sans-serif"><a href="http://www.codeimmersives.com/" style="color:rgb(17,85,204)" target="_blank" data-saferedirecturl="https://www.google.com/url?q=http://www.codeimmersives.com/&amp;source=gmail&amp;ust=1614705860050000&amp;usg=AFQjCNFhf6ldOJYctw7La3Y2aMPF_HIoHA">Code Immersives Homepage</a>&nbsp;</font></div><div style="font-size:small"><font size="2" color="#0c343d" face="tahoma, sans-serif"><a href="https://www.facebook.com/codeimmersives/" style="color:rgb(17,85,204)" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://www.facebook.com/codeimmersives/&amp;source=gmail&amp;ust=1614705860050000&amp;usg=AFQjCNGs74CSFvl9gbCfaUexKAqMszr2Qw">Code Immersives Facebook</a>&nbsp;</font></div><div style="font-size:small"><font size="2" color="#0c343d" face="tahoma, sans-serif"><a href="https://twitter.com/codeimmersives" style="color:rgb(17,85,204)" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://twitter.com/codeimmersives&amp;source=gmail&amp;ust=1614705860050000&amp;usg=AFQjCNEyjym_xCRdhm2WjGW0zSqzYexcyA">Code Immersives Twitter</a>&nbsp;</font></div><div style="font-size:small"><font face="tahoma, sans-serif" style="color:rgb(17,85,204)"><a href="http://www.instagram.com/codeimmersives/" style="color:rgb(17,85,204)" target="_blank" data-saferedirecturl="https://www.google.com/url?q=http://www.instagram.com/codeimmersives/&amp;source=gmail&amp;ust=1614705860050000&amp;usg=AFQjCNGGxg6jq22ezepbXWRpBWdHJD4E5A">Code Immersives Instagram</a></font></div><div style="font-size:small"><font color="#0c343d" face="tahoma, sans-serif"><a href="https://benefits.va.gov/gibill/" style="color:rgb(17,85,204)" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://benefits.va.gov/gibill/&amp;source=gmail&amp;ust=1614705860050000&amp;usg=AFQjCNEZySKbfdYXKRL-YtYi7TpuC4dkmA">GI Bill Benefits Information</a></font></div><div style="font-size:small"><font color="#0c343d" face="tahoma, sans-serif"><a href="https://www.vets.gov/education/apply/" style="color:rgb(17,85,204)" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://www.vets.gov/education/apply/&amp;source=gmail&amp;ust=1614705860050000&amp;usg=AFQjCNEdwf0xvQpmAtIQ4uUNqdQBbwga5Q">Apply for GI Bill Benefits/COE</a></font></div><div style="font-size:small"><a href="https://fafsa.ed.gov/" style="color:rgb(17,85,204);font-family:tahoma,sans-serif" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://fafsa.ed.gov/&amp;source=gmail&amp;ust=1614705860050000&amp;usg=AFQjCNFQhG680zBZ2hzKbAFjM9_-2O0Yrg">Free Application for Federal Student Aid - (FASFA)</a><br></div><div style="font-size:small"><br></div><div style="font-size:small"><font color="#0c343d" face="tahoma, sans-serif">Financial Aid, school code:&nbsp;<b>0 4 2 0 6 2&nbsp; &nbsp; &nbsp;&nbsp;</b></font><span style="color:rgb(12,52,61);font-family:tahoma,sans-serif">Approved VA Facility Code:&nbsp;</span><b style="color:rgb(12,52,61);font-family:tahoma,sans-serif">2 5 3 6 9 1 3 2</b></div><div style="font-size:small"><br></div></div><div style="font-size:12.8px"><font face="tahoma, sans-serif"><font size="2"><b>Confidentiality Notice:</b></font><font size="2"><b>&nbsp;&nbsp;<span style="color:rgb(0,0,0)">This message and its contents are confidential. If you received this message in error, do not use or rely upon it. Instead, please inform the sender and then delete it. Thank you.</span></b></font></font></div></div></div></div></div></div></div></div></div></div></div></div></div></div></div></div></div></div></div></div></div></div></div></div></div></div></div></div></div></div></div>
+	`;
+
 const initial = (firstName, program, upcomingTerm) => {
 	
 	let career = '';
@@ -32,7 +38,8 @@ const initial = (firstName, program, upcomingTerm) => {
 		<br>
 		<br>
 		<p dir="ltr" style="line-height:1.38;margin-top:0pt;margin-bottom:0pt">Warm regards,</p>
-		<p dir="ltr" style="line-height:1.38;margin-top:0pt;margin-bottom:0pt">Jay Vachon</p></div>`;
+		<p dir="ltr" style="line-height:1.38;margin-top:0pt;margin-bottom:0pt">Jay Vachon</p></div>
+		${signature}`;
 
 	return str;
 };
@@ -43,7 +50,7 @@ const repeat = (firstName, program, upcomingTerm) => {
 		<p dir="ltr" style="line-height:1.38;margin-top:0pt;margin-bottom:0pt">I noticed that you filled out the form on our website again indicating your interest in attending one of our coding programs. If you have any questions or if you'd like to proceed with enrollment for our upcoming ${upcomingTerm} term, simply respond to this email and I can start the process with you!</p>
 		<br>
 		<p dir="ltr" style="line-height:1.38;margin-top:0pt;margin-bottom:0pt">Jay</p>
-		`;
+		${signature}`;
 	return str;
 };
 
