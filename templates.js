87562093f4f0470db270ad30edb00d2568e74b41
Jay Vachon <jaylvachon@gmail.com>
2022-01-24 14:20:06 -0500

    update initial email

--- templates.js
+++ templates.js
@@ -25,13 +25,16 @@ const initial = (firstName, program, upcomingTerm) => {
 	
 	let str = `<div dir="ltr"><p dir="ltr" style="line-height:1.38;margin-top:0pt;margin-bottom:0pt">Hello ${firstName},</p>
 		<br>
-		<p dir="ltr" style="line-height:1.38;margin-top:0pt;margin-bottom:0pt">Thank you for your interest in Code Immersives, a one-of-a-kind accredited program covering everything from the fundamentals of programming to full stack development and cloud computing! Please read on for basic information about the program and how to enroll:</p>
+		<p dir="ltr" style="line-height:1.38;margin-top:0pt;margin-bottom:0pt">Thank you for your interest in Code Immersives, a one-of-a-kind software development program covering everything from the fundamentals of programming to full stack development and cloud computing! Please read on for basic information about the program and how to enroll:</p>
+		<br>
+		<p dir="ltr" style="line-height:1.38;margin-top:0pt;margin-bottom:0pt"><b>What courses do you offer?</b></p>
+		<p dir="ltr" style="line-height:1.38;margin-top:0pt;margin-bottom:0pt">We currently offer two courses: a 60-week Associate of Science in Computer Science and Web architecture course and a 45-week Web Development certificate course. Both courses start from the ground up with the fundamentals of programming. They cover front end development with HTML, CSS, and the JavaScript framework React.js; back end development using the JavaScript framework Node.js; and cloud computing with Amazon Web Services. The associate course also includes general education classes and an additional term of coding classes.<p>
 		<br>
 		<p dir="ltr" style="line-height:1.38;margin-top:0pt;margin-bottom:0pt"><b>What's the cost?</b></p>
-		<p dir="ltr" style="line-height:1.38;margin-top:0pt;margin-bottom:0pt">Tuition for the program is $19,995, and we accept various forms of assistance to help cover this cost, including GI Bill benefits, Pell Grants, government student loans and private student loans through Climb.</p>
+		<p dir="ltr" style="line-height:1.38;margin-top:0pt;margin-bottom:0pt">Tuition for the associate course is $26,885, and tuition for the certificate course is $19,995. We accept various forms of assistance to help cover this cost, including GI Bill benefits, Pell Grants, government student loans and private student loans through Climb.</p>
 		<br>
 		<p dir="ltr" style="line-height:1.38;margin-top:0pt;margin-bottom:0pt"><b>What's the schedule?</b></p>
-		<p dir="ltr" style="line-height:1.38;margin-top:0pt;margin-bottom:0pt">Classes run Monday-Thursday, from 10:30 AM to 4:00 PM, with an hour lunch break in the afternoon. Unlike a coding “boot camp,” we take a more measured pace so that everyone with a desire to learn has the opportunity to succeed. In less than a year, you will be prepared to enter the industry as a software developer.</p>
+		<p dir="ltr" style="line-height:1.38;margin-top:0pt;margin-bottom:0pt">Classes run Monday-Thursday, from 10:30 AM to 4:00 PM, with an hour lunch break in the afternoon. Unlike a coding “boot camp,” we take a more measured pace so that everyone with a desire to learn has the opportunity to succeed.</p>
 		<br>
 		<p dir="ltr" style="line-height:1.38;margin-top:0pt;margin-bottom:0pt"><b>How do I enroll?</b></p>
 		<p dir="ltr" style="line-height:1.38;margin-top:0pt;margin-bottom:0pt">We are currently enrolling students in our upcoming ${upcomingTerm} term, and we are offering both online and in-person instruction (for when the coronavirus pandemic subsides). <b>If you are interested in attending, simply reply to this email with an indication that you’d like to proceed</b> and I will send you the relevant materials!</p>
